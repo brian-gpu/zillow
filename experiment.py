9b1c00530f3b0525a9c086c84b71a81691d7348a
brian-gpu <79373206+brian-gpu@users.noreply.github.com>
2021-03-14 11:54:35 -0400

    generalize replace_null_with, add logger option, update log levels, and add spark context helpers

--- experiment.py
+++ experiment.py
@@ -1,73 +1,87 @@
 import os
+import re
 import shutil
 import tarfile
-import logging as app_logger
+import logging
 from pyspark import SparkContext
 
-def replace_nulls_with(source, value):
-        sc = None
+def replace_nulls_with(source, value, sc, logger=logging):
         rdd = None
-        
-        try:
-                sc = SparkContext()
-        except Exception as e:
-                app_logger.info(f'Could not create SparkContext: {e}')
         
         try:
                 rdd = sc.textFile(source)
         except Exception as e:
-                app_logger.info(f'Rdd could not be created from source {source} : {e}')
+                logger.info(f'Rdd could not be created from source {source} : {e}')
         
         try:
-                rdd.map(lambda line: 
-                                tuple(map(lambda field: (field != '')*(field) or value, line.split(','))))
+                rdd = rdd.map(lambda line: 
+                                tuple(map(lambda field: (re.match(r'^[ ]*$', str(field)) != None)*(value) or field, line.split(','))))
         except Exception as e:
-                app_logger.info(f'Error replacing nulls with value {value} : {e}')
-
-        try:
-                sc.stop()
-        except Exception as e:
-                app_logger.info(f'Could not stop SparkContext: {e}')
+                logger.warning(f'Error replacing nulls with value {value} : {e}')
 
         return rdd
 
-def archive_file(source, destination):
+def archive_file(source, destination, logger=logging):
         archive = None
         
         try:
                 os.remove(destination)
         except Exception as e:
-                app_logger.info(f'No file at destination {destination}: {e}')
+                logger.info(f'No file at destination {destination}: {e}')
 
         try:
                 archive = tarfile.open(destination, 'a')
         except Exception as e:
-                app_logger.info(f'Could not create archive: {e}')
+                logger.error(f'Could not create archive: {e}')
 
         try:
                 archive.add(source)
         except Exception as e:
-                app_logger.info(f'Could not archive source {source}: {e}')
+                logger.error(f'Could not archive source {source}: {e}')
         
         try:
                 archive.close()
         except Exception as e:
-                app_logger.info(f'Could not close archive: {e}')
+                logger.error(f'Could not close archive: {e}')
 
-def copy_file(source, destination):
+def copy_file(source, destination, logger=logging):
         try:
                 shutil.copy2(source, destination)
         except Exception as e:
-                app_logger.info(f'Could not copy source {source} to destination {destination} : {e}')
+                logger.error(f'Could not copy source {source} to destination {destination} : {e}')
         
 
-def is_file_empty(source):
+def is_file_empty(source, logger=logging):
         size = 0
         
         try:
                 size = os.stat(source).st_size
         except Exception as e:
-                app_logger.info(f'Could not find file size of source {source}: {e}')
+                logger.info(f'Could not find file size of source {source}: {e}')
         
         return (size == 0)
 
+# Helper functions for managing Spark Contexts
+def start_spark(logger=logging):
+        sc = None
+
+        try:
+                sc = SparkContext()
+        except Exception as e:
+                logger.error(f'Could not create SparkContext: {e}')
+
+        return sc
+
+def stop_spark(sc, logger=logging):
+        try:
+                sc.stop()
+        except Exception as e:
+                logger.error(f'Could not stop SparkContext: {e}')
+
+
+# Example replace_nulls_with, "County_time_series.csv" is in same folder 
+# Once Spark Context is stopped - rdd will be gone
+
+#sc = start_spark()
+#tf = replace_nulls_with('County_time_series.csv', 'Nan', sc)
+#stop_spark(sc)
